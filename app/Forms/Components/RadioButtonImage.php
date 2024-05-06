230b1968ddb2440b5852a8ca53c3078895336ad2
Erfan Ebrahimi <persionhost@gmail.com>
2024-05-06 21:44:16 +0330

    fix radio selection

---
+++ app/Forms/Components/RadioButtonImage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Forms\Components;
+
+use Filament\Forms\Components\Radio;
+
+class RadioButtonImage extends Radio
+{
+    protected string $view = 'forms.components.radio-button-image';
+}
