5be6cb3949d747f6c85f85c4e548339c8a38958d
Erfan Ebrahimi <persionhost@gmail.com>
2024-05-15 20:23:18 +0330

    fix blog

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -16,7 +16,27 @@
     <!-- About Section  -->
     <div id="about-2" class="about-section section-padding pb-60">
         <div class="container">
-            @include('layouts.rr' , ['image_1' => $post->image ? asset('storage/'.$post->image) : null,'image_2' => null,'title' => $post->title,'subTitle' => $post->subTitle . ' |  Created at:'. $post->created_at->diffForHumans(),'content' => $post->content])
+            <div class="row align-items-center">
+                @if($post->image)
+                <div class="col-xl-12 col-lg-12 order-2 order-xl-1">
+                    <div class="about-img-wrap mb-10 text-center">
+                        <div class="image-one wow fadeInUp" data-wow-delay=".2s">
+                            <img src="{{ $post->image ? asset('storage/'.$post->image) : null }}" alt="{{ $post->title }}" style="border-radius: 15px;">
+                        </div>
+                    </div>
+                </div>
+                @endif
+                <div class="col-xl-12 col-lg-12 order-1 order-xl-2">
+                    <div class="about-content-wrap">
+                        <div class="section-title">
+                            <h6>{{ $post->subTitle . ' |  Created at:'. $post->created_at->diffForHumans() }}</h6>
+                            <h2>{{ $post->title }}</h2>
+                        </div>
+                        <p>{!! nl2br(($post->content)) !!}</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
